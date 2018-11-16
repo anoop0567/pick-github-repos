1540b01b80daff5ae2856d5dfade3d6df79814f4
Anoop Chauhan <chauhananu0567@gmail.com>
2018-11-16 21:43:03 +0530

    ascending order and descending order added
    button added for ascending order and descending order based on forks_count and repo star.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent {
   profileData = []; // User Data
   reposData = []; // Repositories Bucket Data
   responseStatus;
-  constructor(http:Http) {
+  constructor(http: Http) {
     http.get(this.url + this.userName)
       .subscribe(response => {
         this.responseStatus = response.status;
@@ -26,10 +26,15 @@ export class AppComponent {
         this.repos = response.json();
       })
   }
-
+ 
+  isClicked:boolean;
+  bothFatched = [false, false];
   // Getting Repositories Data
   getRepos() {
+    this.isClicked = true;
     if (this.userName != '') {
+      this.bothFatched[0] = false;
+      this.bothFatched[1] = false;
       fetch(this.url + this.userName)
         .then(response => {
           this.responseStatus = response.status;
@@ -37,30 +42,70 @@ export class AppComponent {
         })
         .then(data => {
           this.profileData = data;
+          this.bothFatched[0] = true;
+          if(this.bothFatched[0] == true && this.bothFatched[1] == true) {
+            this.isClicked = false;
+          }
         })
-       fetch(this.url + this.userName + '/repos')
+      fetch(this.url + this.userName + '/repos')
         .then(response => {
           this.responseStatus = response.status;
-           return response.json();
+          return response.json();
         })
         .then(data => {
           this.repos = data;
+          this.bothFatched[1] =true;
+          if(this.bothFatched[0] == true && this.bothFatched[1] == true) {
+            this.isClicked = false;
+          }
         })
     }
   }
-  
+
+  reposCount: number;
   // Adding Repositories
   addRepos(repoName, repoURL, language, forks_count, stargazers_count, user_url, user) {
-    this.reposData.unshift({ name: repoName,
-                             html_url: repoURL , 
-                             language : language, 
-                             forks_count : forks_count, 
-                             stargazers_count : stargazers_count,
-                             user_url : user_url,
-                             user : user
-                            })
+    this.reposData.unshift({
+      name: repoName,
+      html_url: repoURL,
+      language: language,
+      forks_count: forks_count,
+      stargazers_count: stargazers_count,
+      user_url: user_url,
+      user: user
+    })
   }
 
+  shortAsc() {
+    this.reposData.sort(function (a, b) {
+      let astar = a.stargazers_count;
+      let bstar = b.stargazers_count;
+      if(a.forks_count === b.forks_count) {
+        if(astar < bstar) {
+          return 1;
+        }else {
+          return -1;
+        }
+      }
+      return a.forks_count - b.forks_count;
+    });
+  }
+
+  shortDsc() {
+    this.reposData.sort(function (a, b) {
+      let astar = a.stargazers_count;
+      let bstar = b.stargazers_count;
+      if(a.forks_count === b.forks_count) {
+        if(astar > bstar) {
+          return 1;
+        }else {
+          return -1;
+        }
+      }
+      return b.forks_count - a.forks_count;
+    });
+    
+  }
   // Removing Repositories
   removeRepos(index) {
     this.reposData.splice(index, 1);
